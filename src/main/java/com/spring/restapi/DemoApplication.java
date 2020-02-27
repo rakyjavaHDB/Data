c42a8f6223849596db0e32621d516df69191b517
rakyjavaHDB <45001815+rakyjavaHDB@users.noreply.github.com>
2020-02-27 13:57:49 +0800

    Update DemoApplication.java

--- src/main/java/com/spring/restapi/DemoApplication.java
+++ src/main/java/com/spring/restapi/DemoApplication.java
@@ -1,10 +1,6 @@
 package com.spring.restapi;
 
-/**
- * @author sahithi
- *Sep 5, 2018
- * 
- */
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
