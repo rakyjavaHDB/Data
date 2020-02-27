8b243577a27e32b8ba67c2c0ab72fb43c35bb911
rakyjavaHDB <45001815+rakyjavaHDB@users.noreply.github.com>
2020-02-27 13:56:05 +0800

    Update Weather.java

--- src/main/java/com/spring/restapi/models/Weather.java
+++ src/main/java/com/spring/restapi/models/Weather.java
@@ -1,10 +1,6 @@
 package com.spring.restapi.models;
 
-/**
- * @author sahithi
- *Sep 6, 2018
- * 
- */
+
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
